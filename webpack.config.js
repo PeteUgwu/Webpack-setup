577abb6b400aeb860d0195af46b2db74ef85e68e
peter <peterugwu64@gmail.com>
2022-11-09 09:04:31 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
